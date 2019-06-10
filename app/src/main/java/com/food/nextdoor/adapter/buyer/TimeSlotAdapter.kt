63cfb50e9df6ec1ee20cdfdce06e3593a6005072
sarith.vasu <sarithvasu@gmail.com>
2019-06-10 17:54:01 +0530

    changed time slot screen

--- app/src/main/java/com/food/nextdoor/adapter/buyer/TimeSlotAdapter.kt
+++ app/src/main/java/com/food/nextdoor/adapter/buyer/TimeSlotAdapter.kt
@@ -1,6 +1,7 @@
 package com.food.nextdoor.adapter.buyer
 
 import android.content.Context
+import android.graphics.Color
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -10,18 +11,36 @@ import com.food.nextdoor.R
 import kotlinx.android.synthetic.main.time_slot_row.view.*
 
 class TimeSlotAdapter(val context: Context, val timeSlots:ArrayList<String>):RecyclerView.Adapter<TimeSlotViewHolder>() {
+    var row_index:Int=-1
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): TimeSlotViewHolder {
         return TimeSlotViewHolder(LayoutInflater.from(context).inflate(R.layout.time_slot_row,p0,false))
     }
 
     override fun getItemCount(): Int {
-      return timeSlots.size
+        return timeSlots.size
     }
 
     override fun onBindViewHolder(holder: TimeSlotViewHolder, position: Int) {
-       holder.tv_time_slot.text=timeSlots.get(position)
-    }
+        holder.tv_time_slot.text=timeSlots.get(position)
 
+
+        holder.tv_time_slot.setOnClickListener {
+            row_index =position
+
+            notifyDataSetChanged()
+        }
+        if(row_index==position){
+            holder.tv_time_slot.setBackgroundColor(Color.BLUE)
+            holder.tv_time_slot.setTextColor(Color.parseColor("#ffffff"))
+        }
+        else{
+            holder.tv_time_slot.setBackgroundColor(Color.parseColor("#ffffff"))
+            holder.tv_time_slot.setTextColor(Color.parseColor("#000000"))
+        }
+
+
+
+    }
 }
 class TimeSlotViewHolder (view: View) : RecyclerView.ViewHolder(view) {
     val tv_time_slot=view.tv_time_slot
